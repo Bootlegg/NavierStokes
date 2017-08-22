9f24aaa6d9c688949ba9b77c0d6f4d688acbfc98
Dan <dankroga@gmail.com>
2017-08-22 22:06:23 +0200

    changed bcs for poisson solver, possibly more correct now

--- BVE/BVEfd.py
+++ BVE/BVEfd.py
@@ -4,6 +4,47 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import axes3d
 
+def SolvePoisson(psi,zeta):
+	"""
+	We set BCs at y = top and y = bottom, but not x, i think
+	Following lec12.pdf here
+	"""
+	#Compute streamfunction from vorticity
+	#ζ_(i,j)^1=(((ψ_(i+1,j)-2ψ_(i,j)+ψ_(i-1,j) ))/dx^2 +((ψ_(i,j+1)-2ψ_(i,j)+ψ_(i,j-1) ))/dy^2 )
+
+	#solve psi from lec12.pdf
+	psin = np.zeros((ny,nx),dtype=float64)
+	psin = psi.copy()
+	
+	#Skal måske lave en special case for enten x = 0 eller x = L, hvor jeg copy over?
+	
+	dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
+	for r in range(500): #pseudo-time
+		psin = psi.copy()
+		psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(
+				+(psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2
+				+(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2
+				-zeta[1:ny-1,1:nx-1])
+
+		psi[1:ny-1,0] = psin[1:ny-1,0]+dtau*(
+				+(psin[1:ny-1,1]-2*psin[1:ny-1,0]+psin[1:ny-1,-1])/dx**2
+				+(psin[2:ny,0]-2*psin[1:ny-1,0]+psin[0:ny-2,0])/dy**2
+				-zeta[1:ny-1,0])
+		#psi[1:ny-1,-1] = psi[1:ny-1,0] 
+		
+		psi[1:ny-1,-1] = psin[1:ny-1,-1]+dtau*(
+		+(psin[1:ny-1,0]-2*psin[1:ny-1,-1]+psin[1:ny-1,-2])/dx**2
+		+(psin[2:ny,-1]-2*psin[1:ny-1,-1]+psin[0:ny-2,-1])/dy**2
+		-zeta[1:ny-1,-1])
+		
+		#boundary at x = L, i'm gonna set equal to x = 0...
+		#Boundary for psi, maybe i should remove these!
+		#psi[:,-1] = 0 #right boundary
+		#psi[:,0] = 0	#left boundary
+		psi[-1,:] = 0
+		psi[0,:] = 0 
+		
+		
 
 Lx = 6000
 Ly = 6000
@@ -27,7 +68,7 @@ zeta = np.array(A*np.exp(-2*(k**2*x**2+m**2*y**2)),dtype=float64)
 zetan = zeta
 #time integration parameters					#hours
 time_end = 3*3600 				#second
-dt = 900
+dt = 100
 
 psi = np.zeros((ny,nx),dtype=float64)
 dypsi = np.zeros((ny,nx),dtype=float64)
@@ -37,27 +78,27 @@ v = np.zeros((ny,nx),dtype=float64)
 dfly = np.zeros((ny,nx),dtype=float64)
 dflx = np.zeros((ny,nx),dtype=float64)
 
-
+SolvePoisson(psi,zeta)
 #Compute streamfunction from vorticity
 #ζ_(i,j)^1=(((ψ_(i+1,j)-2ψ_(i,j)+ψ_(i-1,j) ))/dx^2 +((ψ_(i,j+1)-2ψ_(i,j)+ψ_(i,j-1) ))/dy^2 )
 
-#solve psi from lec12.pdf
-psin = np.zeros((ny,nx),dtype=float64)
-psin = psi.copy()
-dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
-for r in range(500): #pseudo-time
-	psin = psi.copy()
-	psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
-			(psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
-			+(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
-			+zeta[1:ny-1,1:nx-1])
+# #solve psi from lec12.pdf
+# psin = np.zeros((ny,nx),dtype=float64)
+# psin = psi.copy()
+# dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
+# for r in range(500): #pseudo-time
+	# psin = psi.copy()
+	# psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
+			# (psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
+			# +(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
+			# +zeta[1:ny-1,1:nx-1])
 
 
-	#Boundary for psi, maybe i should remove these!
-	psi[:,-1] = 0 #right boundary
-	psi[:,0] = 0	#left boundary
-	psi[-1,:] = 0
-	psi[0,:] = 0 
+	# #Boundary for psi, maybe i should remove these!
+	# #psi[:,-1] = 0 #right boundary
+	# #psi[:,0] = 0	#left boundary
+	# psi[-1,:] = 0
+	# psi[0,:] = 0 
 
 psi+=U0*(Ly/2*1000 - y)
 
@@ -84,6 +125,11 @@ zetan[1:ny-1,1:nx-1] = zeta[1:ny-1,1:nx-1]-dt*(\
 zeta = zetan
 
 
+
+
+
+
+
 #plt.hold(True)
 fig = plt.figure()
 ax = plt.gca()
@@ -102,23 +148,24 @@ def animate(i):
 		+(v[2:ny,1:nx-1]*zeta[2:ny,1:nx-1]-v[0:ny-2,1:nx-1]*zeta[0:ny-2,1:nx-1])/(2*dy)\
 		+beta*v[1:ny-1,1:nx-1])
 	zeta = zetan
+	
+	SolvePoisson(psi,zeta)
+	# #Calculate psi streamfunction
+	# #solve psi from lec12.pdf
+	# psin = np.zeros((ny,nx),dtype=float64)
+	# psin = psi.copy()
+	# dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
+	# for r in range(500): #pseudo-time
+		# psin = psi.copy()
+		# psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
+				# (psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
+				# +(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
+				# +zeta[1:ny-1,1:nx-1])
 
-	#Calculate psi streamfunction
-	#solve psi from lec12.pdf
-	psin = np.zeros((ny,nx),dtype=float64)
-	psin = psi.copy()
-	dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
-	for r in range(500): #pseudo-time
-		psin = psi.copy()
-		psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
-				(psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
-				+(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
-				+zeta[1:ny-1,1:nx-1])
-
-		psi[:,-1] = 0 #right boundary
-		psi[:,0] = 0	#left boundary
-		psi[-1,:] = 0
-		psi[0,:] = 0 
+		# #psi[:,-1] = 0 #right boundary
+		# #psi[:,0] = 0	#left boundary
+		# psi[-1,:] = 0
+		# psi[0,:] = 0 
 
 	psi+=U0*(Ly/2*1000-y)
  	
