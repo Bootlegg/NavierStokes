b817d286957120058a830e68db6a19d443889f37
Dan <dankroga@gmail.com>
2020-07-01 16:34:21 +0200

    Changed comments and added if __name__ style

--- BVE/BVEfd.py
+++ BVE/BVEfd.py
@@ -1,14 +1,20 @@
 """
 Dan Krog
+
+Numerical Solution of Barotropic Vorticity Equation
+
+
+
 Perhaps instead of setting psi = 0, see if it changes if it set psi = psi_old on northern and southern boundary
 for Poisson solver. and hold psi_old constant throughout pseudo-time iteration on nothern and southern boundaries
 
 
-lige nu holder jeg zeta = 0 på boundaries, dette er nok også forkert...
+lige nu holder jeg zeta = 0 på boundaries, maybe
 i hvert fald gøre den periodic etc?
 
 
-måske er min velocity fra stream function psi derivatives også forkerte
+maybe er min velocity fra stream function psi derivatives også forkerte
+
 
 lige nu er beta constant, bør den vidst ikke være, right?
 
@@ -24,7 +30,7 @@ I may have translated matlab code wrong, forgot python numpy slicing doesn't inc
 
 
 import numpy as np
-from pylab import *
+#from pylab import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import axes3d
@@ -38,6 +44,14 @@ from mpl_toolkits.mplot3d import axes3d
 
 def SolvePoisson(psi,zeta):
 	"""
+	Solve Poisson Equation
+	Holton page 465
+	
+	nabla^2psi = zeta
+	
+	where psi is the streamfunction, and zeta is the relative vorticity
+	
+	
 	We set BCs at y = top and y = bottom, but not x, i think
 	Following lec12.pdf here
 	
@@ -56,7 +70,7 @@ def SolvePoisson(psi,zeta):
 	#ζ_(i,j)^1=(((ψ_(i+1,j)-2ψ_(i,j)+ψ_(i-1,j) ))/dx^2 +((ψ_(i,j+1)-2ψ_(i,j)+ψ_(i,j-1) ))/dy^2 )
 
 	#solve psi from lec12.pdf
-	psin = np.zeros((ny,nx),dtype=float64)
+	psin = np.zeros((ny,nx),dtype=np.float64)
 	psin = psi.copy()
 	
 	
@@ -251,7 +265,7 @@ def divflux(P,u,v,dx,dy):
 	#Interessant det der sker ved boundary periodic… vi springer en gridpoint over I centered differences, look it…
 
 def Damping4(Dk4,nx,ny,U):
-	numdif = zeros((ny,nx));          
+	numdif = np.zeros((ny,nx));          
 
 	#Do smoothing in y space for 1st derivative zero at boundaries
 	numdif[3:ny-4,:] = Dk4*(U[5:ny-2,:] -4*U[4:ny-3,:]+6*U[3:ny-4,:]
@@ -276,6 +290,13 @@ def Damping4(Dk4,nx,ny,U):
 	
 def CalcVelocity(u,v,dxpsi,dypsi,psi,dx,dy):
 	"""
+	Calculate velocity (u,v) from stream function psi
+	
+	u = -dpsi/dy
+	v = dpsi/dx
+	
+	Use finite difference
+	
 	More closely following Holton, he does something different on last x coordinate, dxpsi[:,-1]
 	Also note, that we do
 	dxpsi[:,0]=(psi[:,1]-psi[:,-2])/(2*dx)
@@ -328,112 +349,13 @@ def KineticEnergy(u,v):
 	
 	
 def Enstrophy(zeta):
-
+	"""
+	Calculate Enstrophy
+	"""
 	Ens = np.sum(zeta*zeta)
 	return Ens
 	
-Lx = 6000 #km
-Ly = 6000 #km
-nx = 65
-ny = 65
 
-#x = np.linspace(0,Lx,nx)
-#y = np.linspace(0,Ly,ny)
-X = np.linspace(-Lx/2,Lx/2,nx)
-Y = np.linspace(-Ly/2,Ly/2,ny)
-x,y = np.meshgrid(X*1000,Y*1000)
-k = 2*pi/(Lx*1000)
-m = pi/(Ly*1000)
-dx = 1000*Lx/(nx-1)
-dy = 1000*Ly/(ny-1)
-U0 = 20				#zonal wind
-beta = 1.62*10**(-11)	#he set 0 infront?
-Av4 = 10**(-6)
-A = 10**(-4)
-#initial vorticity and streamfunction
-
-zeta0 = np.array(A*np.exp(-2*(k**2*x**2+m**2*y**2)),dtype=float64)
-zeta = zeta0
-zetan = zeta0
-#time integration parameters					#hours
-time_end = 3*3600 				#second
-dt = 100
-
-psi = np.zeros((ny,nx),dtype=float64)
-dypsi = np.zeros((ny,nx),dtype=float64)
-dxpsi = np.zeros((ny,nx),dtype=float64)
-u = np.zeros((ny,nx),dtype=float64)
-v = np.zeros((ny,nx),dtype=float64)
-dfly = np.zeros((ny,nx),dtype=float64)
-dflx = np.zeros((ny,nx),dtype=float64)
-
-SolvePoisson(psi,zeta)
-#Compute streamfunction from vorticity
-#ζ_(i,j)^1=(((ψ_(i+1,j)-2ψ_(i,j)+ψ_(i-1,j) ))/dx^2 +((ψ_(i,j+1)-2ψ_(i,j)+ψ_(i,j-1) ))/dy^2 )
-
-# #solve psi from lec12.pdf
-# psin = np.zeros((ny,nx),dtype=float64)
-# psin = psi.copy()
-# dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
-# for r in range(500): #pseudo-time
-	# psin = psi.copy()
-	# psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
-			# (psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
-			# +(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
-			# +zeta[1:ny-1,1:nx-1])
-
-
-	# #Boundary for psi, maybe i should remove these!
-	# #psi[:,-1] = 0 #right boundary
-	# #psi[:,0] = 0	#left boundary
-	# psi[-1,:] = 0
-	# psi[0,:] = 0 
-
-psi+=U0*(Ly/2*1000 - y)
-
-#Calculate gradients for air velocity
-#forward on bottom? Backward on top?
-# dypsi[0,:] = (psi[1,:] - psi[0,:])/dy
-# dypsi[-1,:] = (psi[-1,:] - psi[-2,:])/dy
-# dypsi[1:-2,:] = (psi[2:-1,:]-psi[0:-3,:])/(2*dy)  #Centered difference
-
-
-# dxpsi[:,0]=(psi[:,1]-psi[:,-1])/(2*dx)
-# dxpsi[:,-1]= dxpsi[:,0]#(psi[:,0]-psi[:,-2])/(2*dx)
-# dxpsi[:,1:-2]= (psi[:,2:-1]-psi[:,0:-3])/(2*dx)  #centered difference
-
-# u = -dypsi
-# v = dxpsi
-
-u,v = CalcVelocity(u,v,dxpsi,dypsi,psi,dx,dy)
-
-dflx,dfly = divflux(zeta,u,v,dx,dy)
-
-#Forward time difference
-FirstStepZeta(zeta0,zeta,u,v,beta)
-# zetan[1:ny-1,1:nx-1] = zeta[1:ny-1,1:nx-1]-dt*(\
-	# (u[1:ny-1,2:nx]*zeta[1:ny-1,2:nx]-u[1:ny-1,0:nx-2]*zeta[1:ny-1,0:nx-2])/(2*dx)\
-	# +(v[2:ny,1:nx-1]*zeta[2:ny,1:nx-1]-v[0:ny-2,1:nx-1]*zeta[0:ny-2,1:nx-1])/(2*dy)\
-	# +beta*v[1:ny-1,1:nx-1])
-# zeta = zetan
-
-
-
-
-t=0
-t += dt
-#plt.hold(True)
-fig = plt.figure()
-ax = plt.gca()
-#ax.contour(x,y,zeta,colors='black')
-#ax.quiver(x,y,u,v)
-ax.set_title('Barotropic Vorticity Equation')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-#ax.set_xticks([0])
-#ax.set_yticks([0])
-#ax.set_xticks([Lx/3,0],["hey","loL"])
-#ax.set_yticks([Ly/3,2*Ly/3],[1,1])
 
 def animate(i):
 
@@ -521,12 +443,120 @@ def animate(i):
 		fig.savefig('BVE.png', bbox_inches='tight')
 
 
+if __name__ == "__main__":
+	
+	
+	
+	Lx = 6000 #km
+	Ly = 6000 #km
+	nx = 65
+	ny = 65
+	pi = 3.141592
+	#x = np.linspace(0,Lx,nx)
+	#y = np.linspace(0,Ly,ny)
+	X = np.linspace(-Lx/2,Lx/2,nx)
+	Y = np.linspace(-Ly/2,Ly/2,ny)
+	x,y = np.meshgrid(X*1000,Y*1000)
+	k = 2*pi/(Lx*1000)
+	m = pi/(Ly*1000)
+	dx = 1000*Lx/(nx-1)
+	dy = 1000*Ly/(ny-1)
+	U0 = 20				#zonal wind
+	beta = 1.62*10**(-11)	#he set 0 infront?
+	Av4 = 10**(-6)
+	A = 10**(-4)
+	#initial vorticity and streamfunction
 
-anim = animation.FuncAnimation(
-fig, 
-animate, 
-#frames=5, 
-interval=0.5,
-blit=False #blit=False default, 
-)
-plt.show()
+	zeta0 = np.array(A*np.exp(-2*(k**2*x**2+m**2*y**2)),dtype=np.float64)
+	zeta = zeta0
+	zetan = zeta0
+	#time integration parameters					#hours
+	time_end = 3*3600 				#second
+	dt = 100
+
+	psi = np.zeros((ny,nx),dtype=np.float64)
+	dypsi = np.zeros((ny,nx),dtype=np.float64)
+	dxpsi = np.zeros((ny,nx),dtype=np.float64)
+	u = np.zeros((ny,nx),dtype=np.float64)
+	v = np.zeros((ny,nx),dtype=np.float64)
+	dfly = np.zeros((ny,nx),dtype=np.float64)
+	dflx = np.zeros((ny,nx),dtype=np.float64)
+
+	SolvePoisson(psi,zeta)
+	#Compute streamfunction from vorticity
+	#ζ_(i,j)^1=(((ψ_(i+1,j)-2ψ_(i,j)+ψ_(i-1,j) ))/dx^2 +((ψ_(i,j+1)-2ψ_(i,j)+ψ_(i,j-1) ))/dy^2 )
+
+	# #solve psi from lec12.pdf
+	# psin = np.zeros((ny,nx),dtype=float64)
+	# psin = psi.copy()
+	# dtau = 0.5*0.5*(0.5*dx**2+0.5*dy**2)
+	# for r in range(500): #pseudo-time
+		# psin = psi.copy()
+		# psi[1:ny-1,1:nx-1] = psin[1:ny-1,1:nx-1]+dtau*(\
+				# (psin[1:ny-1,2:nx]-2*psin[1:ny-1,1:nx-1]+psin[1:ny-1,0:nx-2])/dx**2\
+				# +(psin[2:ny,1:nx-1]-2*psin[1:ny-1,1:nx-1]+psin[0:ny-2,1:nx-1])/dy**2\
+				# +zeta[1:ny-1,1:nx-1])
+
+
+		# #Boundary for psi, maybe i should remove these!
+		# #psi[:,-1] = 0 #right boundary
+		# #psi[:,0] = 0	#left boundary
+		# psi[-1,:] = 0
+		# psi[0,:] = 0 
+
+	psi+=U0*(Ly/2*1000 - y)
+
+	#Calculate gradients for air velocity
+	#forward on bottom? Backward on top?
+	# dypsi[0,:] = (psi[1,:] - psi[0,:])/dy
+	# dypsi[-1,:] = (psi[-1,:] - psi[-2,:])/dy
+	# dypsi[1:-2,:] = (psi[2:-1,:]-psi[0:-3,:])/(2*dy)  #Centered difference
+
+
+	# dxpsi[:,0]=(psi[:,1]-psi[:,-1])/(2*dx)
+	# dxpsi[:,-1]= dxpsi[:,0]#(psi[:,0]-psi[:,-2])/(2*dx)
+	# dxpsi[:,1:-2]= (psi[:,2:-1]-psi[:,0:-3])/(2*dx)  #centered difference
+
+	# u = -dypsi
+	# v = dxpsi
+
+	u,v = CalcVelocity(u,v,dxpsi,dypsi,psi,dx,dy)
+
+	dflx,dfly = divflux(zeta,u,v,dx,dy)
+
+	#Forward time difference
+	FirstStepZeta(zeta0,zeta,u,v,beta)
+	# zetan[1:ny-1,1:nx-1] = zeta[1:ny-1,1:nx-1]-dt*(\
+		# (u[1:ny-1,2:nx]*zeta[1:ny-1,2:nx]-u[1:ny-1,0:nx-2]*zeta[1:ny-1,0:nx-2])/(2*dx)\
+		# +(v[2:ny,1:nx-1]*zeta[2:ny,1:nx-1]-v[0:ny-2,1:nx-1]*zeta[0:ny-2,1:nx-1])/(2*dy)\
+		# +beta*v[1:ny-1,1:nx-1])
+	# zeta = zetan
+
+
+
+
+	t=0
+	t += dt
+	#plt.hold(True)
+	fig = plt.figure()
+	ax = plt.gca()
+	#ax.contour(x,y,zeta,colors='black')
+	#ax.quiver(x,y,u,v)
+	ax.set_title('Barotropic Vorticity Equation')
+	ax.set_xlabel('x')
+	ax.set_ylabel('y')
+	#ax.set_xticks([0])
+	#ax.set_yticks([0])
+	#ax.set_xticks([Lx/3,0],["hey","loL"])
+	#ax.set_yticks([Ly/3,2*Ly/3],[1,1])
+
+	
+
+	anim = animation.FuncAnimation(
+	fig, 
+	animate, 
+	#frames=5, 
+	interval=0.5,
+	blit=False #blit=False default, 
+	)
+	plt.show()
